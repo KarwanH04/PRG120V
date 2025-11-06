365b5d53ae052afe9fe77f853bd1cb6fe0054230
KarwanH04 <Karwan.Hame@hotmail.com>
2025-11-06 01:51:16 +0100

    iS

--- Innlevering2/db.php
+++ Innlevering2/db.php
@@ -1,11 +1,17 @@
-<?php /* db-tilkobling */
+<?php
+/* db-tilkobling.php */
 /*
-/* Programmet foretar tilkobling til database-server og valg av database
+  Programmet kobler til database-serveren og velger databasen.
+  Denne versjonen fungerer lokalt uten miljøvariabler (XAMPP, MAMP, Linux, etc.)
 */
-$host = getenv('DB_HOST');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
-$database = getenv('DB_DATABASE');
-$db=mysqli_connect($host,$username,$password,$database) or die ("ikke kontakt med database-server");
-/* tilkobling til database-serveren utført */
+
+$host = getenv('DB_HOST') ?: "127.0.0.1";     // bruker miljøvariabel hvis satt, ellers localhost
+$username = getenv('DB_USER') ?: "root";      // standard brukernavn i XAMPP/MAMP
+$password = getenv('DB_PASSWORD') ?: "";      // legg inn passord hvis du har et
+$database = getenv('DB_DATABASE') ?: "skole"; // standard databasenavn, bytt hvis du bruker et annet
+
+$db = mysqli_connect($host, $username, $password, $database)
+      or die("Ikke kontakt med database-server eller databasen finnes ikke");
+
+/* Tilkobling til database-serveren utført */
 ?>
